37c4a25313302da2922fa7ce550f779b5f5d6a3d
Ferry <ferry@data-project.nl>
2026-04-08 13:38:45 +0200

    Initial Commit

---
+++ OVMDivePlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OVMDivePlanner
+//
+//  Created by Ferry Ouwerkerk on 08/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OVMDivePlanner/OVMDivePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OVMDivePlannerApp.swift
+//  OVMDivePlanner
+//
+//  Created by Ferry Ouwerkerk on 08/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct OVMDivePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
